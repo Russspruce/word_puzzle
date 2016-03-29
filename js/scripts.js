d4323d3a6a49f28499b351cf6b1de9ac5cce8e77
Andrew Russo and Niem Nguyen <niem@>
2016-03-29 11:08:44 -0700

    Fixed code.

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,11 @@ $(document).ready(function() {
 $("#sentenceReplace form").submit(function(event) {
   event.preventDefault();
 
-
-
   var sentence = $("#sentenceToSolve").val();
   var i=0, sentenceLength=sentence.length;
-
   for(i; i < sentenceLength; i++) {
 
-    sentence = sentence.replace ("a", "-").replace("e", "-").replace("i", "-").replace("o", "-").replace("u", "-")("A", "-").replace("E", "-").replace("I", "-").replace("O", "-").replace("U", "-");
+    sentence =sentence.replace ("a", "-").replace("e", "-").replace("i", "-").replace("o", "-").replace("u", "-").replace("A", "-").replace("E", "-").replace("I", "-").replace("O", "-").replace("U", "-");
 
   }
     $(".form-group").addClass("hide");
