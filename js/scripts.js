e36f43aacd8d1db82ebba1de36822d663d46c63a
Andrew Russo and Niem Nguyen <niem@>
2016-03-29 14:58:32 -0700

    shorter way of filtering vowels with regular expression

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,9 @@ $("#sentenceReplace form").submit(function(event) {
   var sentence = $("#sentenceToSolve").val();
   var i=0, sentenceLength=sentence.length;
   for(i; i < sentenceLength; i++) {
+    sentence = sentence.replace(/[a,e,i,o,u]/ig,"-");
 
-    sentence =sentence.replace ("a", "-").replace("e", "-").replace("i", "-").replace("o", "-").replace("u", "-").replace("A", "-").replace("E", "-").replace("I", "-").replace("O", "-").replace("U", "-");
+    // sentence =sentence.replace ("a", "-").replace("e", "-").replace("i", "-").replace("o", "-").replace("u", "-").replace("A", "-").replace("E", "-").replace("I", "-").replace("O", "-").replace("U", "-");
 
   }
     $(".form-group").addClass("hide");
